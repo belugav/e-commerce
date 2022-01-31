ba5389769cca15ad71410dd9878feac5e7b37c36
belu gavito <mbgavito@gmail.com>
2022-01-31 19:02:37 -0300

    proyecto final

--- src/pages/CustomerPage/CustomerPage.js
+++ src/pages/CustomerPage/CustomerPage.js
@@ -11,28 +11,33 @@ export default function CostumerPage()
        <div>
     
     <div className="contenedorFoto">
+        
         <img className="imagenTestimonio" src="assets/testimonio1.jpg"></img>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,</p>
+        
+        <p>Sofia Gomez, se fue a New York y nos compartió esta foto en el puente de Brooklyn
+            y esto nos conto "es la primera vez que vengo a esta gran ciudad y es mas linda que en las peliculas, muchas gracias traveller".
+        </p>
         </div>
     <div className="contenedorFoto">
+        
         <img className="imagenTestimonio" src="assets/testimonio2.jpg"></img>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,</p>
+        <p>Este grupo de amigas también visitó Nueva York y se los puede ver en Times Square, una de ellas nos dijo "muchas gracias traveller gracias a ustedes pudimos cumplir el sueño de viajar juntas."</p>
         </div>
     <div className="contenedorFoto">
         <img className="imagenTestimonio" src="assets/testimonio3.jpg"></img>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,</p>
+        <p>La familia Perez se fue a Paris, se los puede ver con la torre eiffel de fondo y muy felices con su hija Amelia de tan solo 1 año. </p>
         </div>
     <div className="contenedorFoto">
         <img className="imagenTestimonio" src="assets/testimonio4.jpg"></img>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,</p>
+        <p>Acá nos encontramos con Mica que cumplio el sueño de poder ir a Italia y andar en gondola en la increíble ciudad de Venecia</p>
         </div>
     <div className="contenedorFoto">
         <img className="imagenTestimonio" src="assets/testimonio5.jpg"></img>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,</p>
+        <p>Esta pareja de recién casados Nico y Vicky, fueron a Roma donde se sacaron esta maravillosa foto en la Fontana de Trevi. Muy enamorados, pasando unos lindos dias en esta enrome ciudad.</p>
         </div>
    <div className="contenedorFoto">
       <img className="imagenTestimonio" src="assets/testimonio6.jpg"></img>
-      <p>LoLorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,</p>
+      <p>Raul luego de tanto planearlo pudo ir a visitar a su hijo a Australia, mas especificamente a Sydney donde se lo puede ver muy contento.</p>
        </div>
     
     
